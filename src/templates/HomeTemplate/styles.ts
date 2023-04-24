ea186ea228246ee42ae1499f8ea524a2e43d84a4
Evandro Calado <evandrocalado07@gmail.com>
2023-04-24 10:36:15 -0300

    add grid on styles HomeTemplate

--- src/templates/HomeTemplate/styles.ts
+++ src/templates/HomeTemplate/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(28rem, 1fr));
+    gap: ${theme.spacings.large};
+  `}
 `;
