cf35b3c5af67df1f22c456610a7822a968ebe1d6
Evandro Calado <evandrocalado07@gmail.com>
2023-04-26 21:06:53 -0300

    fix fonts of project

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -20,6 +20,10 @@ export const GlobalStyles = createGlobalStyle`
   h1, h2, h3, h4, h5, h6 {
     font-family: ${({ theme }) => theme.font.family.secondary};
   }
+
+  p {
+    font-family: ${({ theme }) => theme.font.family.default};
+  }
  
   ul, ol {
     margin: ${({ theme }) => theme.spacings.medium};
