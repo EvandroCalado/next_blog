829cc48ef9ddd38ff01ec1ec39b8ddd3af047458
evandrocalado <evandrocalado07@gmail.com>
2023-05-06 18:06:12 -0300

    change styles of Card

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,35 +1,40 @@
 import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
-  ${({ theme }) => css`
-    -webkit-box-shadow: 0px 4px 13px -1px rgba(0,0,0,0.34);
-    -moz-box-shadow: 0px 4px 13px -1px rgba(0,0,0,0.34);
-    box-shadow: 0px 4px 13px -1px rgba(0,0,0,0.34);
-    border-radius: ${theme.spacings.xsmall};
-    overflow: hidden;
-  `}
+    a {
+      transition: all 300ms ease-in-out;
+    }
+
+    a:hover {
+      opacity: 1;
+      filter: brightness(170%);
+    }
 `;
 
 export const Cover = styled.div`
+  ${({ theme }) => css`
     img {
       width: 100%;
       display: block;
       aspect-ratio: 16/9;
+      border-radius: .5rem;
+      margin-bottom: ${theme.spacings.small};
+
     }
+  `}
 `;
 
 export const Heading = styled.h2`
   ${({ theme }) => css`
-    padding: 1rem;
     font-size: ${theme.font.sizes.smallMedium};
     color: ${theme.colors.darkGray};
+    margin-bottom: ${theme.spacings.xsmall};
   `}
 `;
 
 export const Paragraph = styled.p`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.small};
-    padding: 0 1rem;
     margin-bottom: ${theme.spacings.small};
     color: ${theme.colors.primary};
   `}
